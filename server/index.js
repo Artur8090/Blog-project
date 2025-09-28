2b332b3b0615e75656926b50d7e168d1efa83284
Artur8090 <artur.cheuchiev@gmail.com>
2025-09-28 15:22:23 +0200

    blog project

---
+++ server/index.js
@@ -0,0 +1,43 @@
+import express, { json } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import authRouter from "./routes/auth.js";
+
+const app = express();
+
+dotenv.config();
+
+
+// Constants
+const PORT = process.env.PORT || 3001;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.json({ message: "All is fine" });
+})
+
+//Routes
+
+app.use("/api/auth", authRouter)
+async function start() {
+    try{
+        await mongoose.connect(
+            `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_NAME}.gaxvoxd.mongodb.net/?retryWrites=true&w=majority&appName=${DB_NAME}`
+        )
+
+        app.listen(3001, () => {
+            console.log("Server started on port 3001");
+        });
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+start()
---
+++ server/routes/auth.js
@@ -0,0 +1,15 @@
+import Router from "express";
+import {register, login, getMe} from "../controllers/auth.js";
+const router = new Router();
+
+// Registration
+// http://localhost:3001/api/auth/register
+router.post("/register", register);
+
+// Login
+// http://localhost:3001/api/auth/login
+router.post("/login", login);
+// Get me
+// http://localhost:3001/api/auth/me
+router.get("/me", getMe);
+export default router;
