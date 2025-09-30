cf2ed2676b94fc14d3e00e3bd31c2228d6fc00a0
Artur8090 <artur.cheuchiev@gmail.com>
2025-09-30 21:02:05 +0200

    Fixed issue with older version of node package, added a couple new pages

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
-
+import { Link, link, NavLink } from "react-router-dom"
 export const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    const activeStyles = {
+        color: 'white'
+    }
+    const isAuth = false;
+    return (
+        <div className="flex items-center justify-between py-4 w-full">
+            <span className="flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm mr-6">
+                E
+            </span>
+            {
+                isAuth && (
+
+                    <ul className="flex gap-8 flex-1">
+                        <li><NavLink to={'/'} href="/" className="text-xs text-gray-400 hover:text-white" style={({ isActive }) => isActive ? activeStyles : undefined}>Главная</NavLink></li>
+                        <li><NavLink to={'/posts'} href="/" className="text-xs text-gray-400 hover:text-white" style={({ isActive }) => isActive ? activeStyles : undefined}>Мои посты</NavLink></li>
+                        <li><NavLink to={'/new'} href="/" className="text-xs text-gray-400 hover:text-white" style={({ isActive }) => isActive ? activeStyles : undefined}>Добавить пост</NavLink></li>
+                    </ul>
+                )}
+            <div className="flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm px-4 ml-6">
+                {isAuth ? (<button>Выйти</button>) : (
+                    <Link to={'/login'}>Войти</Link>
+                )}
+            </div>
+
+        </div>
+    );
 }
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,4 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return <form onSubmit={e => e.preventDefault()} className = "w-1/4 h-60 mx-auto mt-40">
+
+    <h1 className="text-lg text-white text-center">Авторизация</h1>
+    <label className="text-xs text-gray-400">
+        Username:
+        <input type="text" placeholder="Username" className="mt-1 text-black w-full bg-gray-400 px-2 py-1 text-xs rounded-sm outline-none placeholder:text-gray-400"/> 
+    </label>
+        <label className="text-xs text-gray-400">
+        Password:
+        <input type="text" placeholder="Password" className="mt-1 text-black w-full bg-gray-400 px-2 py-1 text-xs rounded-sm outline-none placeholder:text-gray-400"/> 
+    </label>
+
+    <div className="flex gap-8 justify-center mt-4">
+        <button type="submit" className="flex justify-center bg-gray-600 items-center text-xs text-white rounder-sm py-w px-4">
+            Войти
+        </button>
+        <Link to='/register' className='flex justify-center items-center text-xs text-white'>Нет Аккаунта?</Link>
+    </div>
+  </form>;
 }
